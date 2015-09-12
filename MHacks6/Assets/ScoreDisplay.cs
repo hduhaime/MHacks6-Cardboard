186dca7c677ced63785115e2d567d8101252ceaf
hduhaime <hduhaime@umich.edu>
2015-09-12 18:20:28 -0400

    Scene + HUD

---
+++ MHacks6/Assets/ScoreDisplay.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    private int score = 0;
+
+    public Canvas scoreCanvas;
+
+    private Text scoreText;
+
+    public Text scoreText2;
+
+
+	// Use this for initialization
+	void Start () {
+
+        scoreText = scoreCanvas.GetComponent<Text>();
+        scoreText.text = scoreText2.text = "Score: 0";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space")) addPoints(10);
+
+        scoreText.text = scoreText2.text = "SCORE: " + score;
+
+
+    }
+
+    public void addPoints(int points) { score += points;  }
+}
