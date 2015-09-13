70f6bb7ad581f8b7ec76c3d5869929f0e4643564
hduhaime <hduhaime@umich.edu>
2015-09-12 23:30:26 -0400

    Don't make me say fuck

--- MHacks6/Assets/Cardboard/Scripts/PlayerController.cs
+++ MHacks6/Assets/Cardboard/Scripts/PlayerController.cs
@@ -3,17 +3,23 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    Animator animator;
+
     public GameObject shot;
     public Transform shotSpawn;
     public Cardboard player;
 	// Use this for initialization
 	void Start () {
+        animator = transform.Find("Head").Find("Main Camera").Find("Gun_2").GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (player.Triggered){
+            animator.ResetTrigger("ShotFire");
+            animator.SetTrigger("ShotFire");
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            
         }
         
 	}
